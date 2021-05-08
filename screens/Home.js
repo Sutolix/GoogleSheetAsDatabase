10973c46cea18c2bd50d4e246444297b548f96a2
Sutolix <tg884ght@gmail.com>
2021-05-08 11:36:33 -0300

    add Header

--- screens/Home.js
+++ screens/Home.js
@@ -1,32 +1,39 @@
 import React from 'react'
+import Head from 'next/head'
 
 function Home({ jogos }) {
   return (
-    <main className="container">
-      <h1>Google Sheets</h1>
-      
-      <ol className="items">
-        {jogos.map((jogo, indice) => {
-          const posicao = indice + 1
+    <>
+      <Head>
+        <title>Google Sheets</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <main className="container">
+        <h1>Google Sheets</h1>
+        
+        <ol className="items">
+          {jogos.map((jogo, indice) => {
+            const posicao = indice + 1
 
-          return (
-            <li key={jogo.nome} className="item-row">
-              <div className="item-title">
-                <span className="item-position">{posicao}</span>
-                <span className="item-divider"> - </span>
-                <span className="item-name">{jogo.nome}</span>
-              </div>
-              <div className="item-info">
-                <div className="item-image">
-                  <img src={jogo.imagem_capa} alt={`Capa do jogo: ${jogo.nome}`} />
+            return (
+              <li key={jogo.nome} className="item-row">
+                <div className="item-title">
+                  <span className="item-position">{posicao}</span>
+                  <span className="item-divider"> - </span>
+                  <span className="item-name">{jogo.nome}</span>
                 </div>
-                <p className="item-description">{jogo.descricao}</p>
-              </div>
-            </li>
-          )
-        })}
-      </ol>
-    </main>
+                <div className="item-info">
+                  <div className="item-image">
+                    <img src={jogo.imagem_capa} alt={`Capa do jogo: ${jogo.nome}`} />
+                  </div>
+                  <p className="item-description">{jogo.descricao}</p>
+                </div>
+              </li>
+            )
+          })}
+        </ol>
+      </main>
+    </>
   )
 }
 
